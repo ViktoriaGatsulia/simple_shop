aca3e9d1d77b7734bfed71a5ba22630ce4665cd8
ViktoriaGatsulia <viktoria.gatsulia@gmail.com>
2020-09-02 00:22:02 +0700

    Init project

---
+++ src/main/java/ru/example/App.java
@@ -0,0 +1,12 @@
+package ru.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
+
+/*
+    $ mvn compile && mvn package
+    $ java -jar target/simple_shop-1.0-SNAPSHOT.jar
+ */
